060718c7b36c1e75f7eba5a44b7e217210ce2060
Akos002 <papphegyiakos2007@gmail.com>
2024-11-05 09:31:39 +0100

    alapok

--- metodusok/Program.cs
+++ metodusok/Program.cs
@@ -6,6 +6,16 @@
         {
             Koszonto();
             Console.WriteLine(Osszeadas(10,90));
+            int a = 5;
+            Console.WriteLine(Duplaz(a));
+            int duplazott = Duplaz(a);
+            Console.WriteLine(a);
+            Console.WriteLine(duplazott);
+            Console.WriteLine(Osszeg(10,7));
+            int elso = 5;
+            int masodik = 10;
+            Csere(elso, masodik);
+            Console.WriteLine(elso + " " + masodik);
         }
 
         static void Koszonto()
@@ -17,6 +27,23 @@
         static int Osszeadas(int a, int b)
         {
             return a + b;
+        }
+
+        static int Duplaz(int szam)
+        {
+            return szam * 2;
+        }
+
+        static int Osszeg(int a, int b = 5)
+        {
+            return a + b;
+        }
+
+        static void Csere(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
         }
 
     }
