3f16e2c400c9e6a159a5db9bf6af8696e1662c77
Akos002 <papphegyiakos2007@gmail.com>
2024-11-05 09:01:51 +0100

    kezdeti

--- metodusok/Program.cs
+++ metodusok/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace metodusok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Koszonto();
+            Console.WriteLine(Osszeadas(10,90));
+        }
+
+        static void Koszonto()
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+
+        static int Osszeadas(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+}
